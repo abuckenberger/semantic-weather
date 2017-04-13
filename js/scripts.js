06c87b098e0a2c009bc96187705aa557565ab891
abuckenberger <abuckenberger@vcd-ceb131-24.eastern.ewu.edu>
2017-04-13 16:05:16 -0700

    SEMANTIC UI
    *added framework
    *added accordian/columns
    *got the weather working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 // SCRIPTS
 
-console.log('hello abby!');
+
+$.simpleWeather({
+    location: 99004,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('.image img').attr('src', weather.image);
+      $('.description p').text(weather.text);
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  });
+
+// accordian
+
+$('.ui.accordion')
+  .accordion()
+;
